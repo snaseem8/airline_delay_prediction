1a41a306ed79c9021cef06815b310a432444cfae
swise30 <82410530+swise30@users.noreply.github.com>
2025-04-09 10:31:23 -0400

    saving

---
+++ data_pca.py
@@ -0,0 +1,8 @@
+from pca import PCA
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+# from sklearn.decomposition import PCA
